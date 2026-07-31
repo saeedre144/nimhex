d980a87ef854a20a183039ff6b4828b7e1c7699f
saeed <saeedre144@live.com>
2026-07-31 14:32:11 +0330

    Scaffold Laravel API and Vue SPA, confirm CORS-enabled connectivity

---
+++ api/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/ping', fn () => response()->json(['message' => 'hello from Laravel']));
